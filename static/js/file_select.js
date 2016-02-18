be294f5a6eaef8634a1b096bb6271fac4eca06d1
asdf <wennis@rpi.edu>
2016-02-18 11:11:14 -0500

    Added URL existence checking

--- static/js/file_select.js
+++ static/js/file_select.js
@@ -1,4 +1,15 @@
 //Capture the URL from the user via a pop-up text box
 function GetFileURL() {
     var URL = prompt("Please enter the URL of the file you want to upload");
+    if (!URLExists(URL)) {
+        alert("Invalid URL!!!"); 
+    }
+}
+
+function URLExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    alert(http.status);
+    return http.status!=404;
 }
